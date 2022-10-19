1860d91b70390acbcbb0b00b797d6d992b16d1cb
Paulo Araujo <pauloaraujo.phz@gmail.com>
2022-10-19 16:04:29 -0300

    Atualização dos Projetos

--- pages/Eshop.jsx
+++ pages/Eshop.jsx
@@ -12,7 +12,7 @@ const criptobase = () => {
         <Image className='absolute z-1' layout='fill' objectFit='cover' src={eshop} alt='/' />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] 
           translate-y-[-50%] text-white z-10 p-2'>
-          <h2 className='py-2'>Criptobase APP</h2>
+          <h2 className='py-2'>eShop React eCommerce</h2>
           <h3>React JS / Redux / Firebase</h3>
         </div>
       </div>
